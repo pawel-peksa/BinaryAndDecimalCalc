b87f9ed40b6849fda66c4161b186ed446d74edb6
Paweł <pawelpeksa@gmail.com>
2021-10-28 20:36:59 +0200

    create validation() and allow only one digit for a box

--- js/DecCalculator.js
+++ js/DecCalculator.js
@@ -23,9 +23,7 @@ class DecCalculator extends Calculator {
     return result;
   }
 
-  initEvents() {
-    super.initEvents();
-
+  validateAndFormat() {
     const boxes =
       this.$calculatorDOMElement.querySelectorAll(".display-number");
     const toolTip = this.$calculatorDOMElement.querySelector(".popover");
@@ -34,18 +32,22 @@ class DecCalculator extends Calculator {
     toolTip.lastElementChild.innerText = "Naciśnij aby dodać wartości";
 
     //show tooltip when box clicked
+    //clear box when clicked
     [...boxes].forEach((box) => {
-      box.addEventListener("click", () => {
+      box.firstElementChild.addEventListener("click", (e) => {
         toolTip.classList.add("show");
+        e.currentTarget.innerText = "";
       });
     });
 
     //allow only one char in the box
     [...boxes].forEach((box) => {
-      box.addEventListener("keyup", (e) => {
-        if (e.target.innerText.length > 1) {
-          e.target.innerText = e.target.innerText.substr(0, 1);
-        }
+      box.addEventListener("keydown", (e) => {
+        e.preventDefault();
+        e.target.innerText = e.key;
+        // if (e.target.innerText.length > 1) {
+        //   e.target.innerText = e.target.innerText.substr(0, 1);
+        // }
       });
     });
 
@@ -53,15 +55,12 @@ class DecCalculator extends Calculator {
     [...boxes].forEach((box) => {
       box.addEventListener("focusout", (e) => {
         if (![0, 1, 2, 3, 4, 5, 6, 7, 8, 9].includes(+e.target.innerText)) {
-          //edit tooltip message
           toolTip.lastElementChild.innerText =
             "Wartości muszą być cyframi (0-9)";
           toolTip.style.backgroundColor = "#ffe6e6";
-
           //edit wrong box
           e.target.style.color = "#450000";
           e.target.focus();
-
           //hide +
           toolTip.previousElementSibling.style.visibility = "hidden";
         } else {
@@ -73,6 +72,21 @@ class DecCalculator extends Calculator {
         }
       });
     });
+
+    // Put 0 when focusout empty box
+    [...boxes].forEach((box) => {
+      box.addEventListener("focusout", (e) => {
+        if (e.target.innerText === "") {
+          e.target.innerText = "0";
+        }
+      });
+    });
+  }
+
+  initEvents() {
+    super.initEvents();
+
+    this.validateAndFormat();
 
     // Run calculator
     this.$calculatorDOMElement
